0df78085d7c97fa4c9901fd22b87c11549a20314
Nisheshchy <nisheshchy2022@gmail.com>
2026-02-15 23:35:40 +0545

    Quiz app for start coding

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import Quiz from "./Components/Quiz/Quiz";
+
+const App = () => {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,57 @@
+/** @format */
+
+import React, { useState } from "react";
+import "./Quiz.css";
+import { data } from "../../assets/data";
+
+const Quiz = () => {
+  let [index, setIndex] = useState(1);
+  let [question, setQuestion] = useState(data[index]);
+  const checkAns = (e, ans) => {
+    if (question.ans === ans) {
+      e.target.classList.add("correct");
+    } else {
+      e.target.classList.add("wrong");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Wellcome to Quiz app</h1>
+      <hr />
+      <h2>
+        {index + 1}. {question.question}
+      </h2>
+      <ul>
+        <li
+          onClick={(e) => {
+            checkAns(e, 1);
+          }}>
+          {question.option1}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 2);
+          }}>
+          {question.option2}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 3);
+          }}>
+          {question.option3}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 4);
+          }}>
+          {question.option4}
+        </li>
+      </ul>
+      <button>Next</button>
+      <div className="index">1of 5 questions</div>
+    </div>
+  );
+};
+
+export default Quiz;
